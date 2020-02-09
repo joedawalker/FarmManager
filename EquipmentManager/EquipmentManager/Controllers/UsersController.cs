f86df02cbf1c62bf227fd144c5acc44267652afe
Joseph Walker <joedawalker@gmail.com>
2020-02-08 20:00:36 -0700

    Added two templated controllers and added the swagger documentation framework.

---
+++ EquipmentManager/EquipmentManager/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UserService;
+using System.Text.Json;
+
+namespace EquipmentManager.Controllers
+{
+    [Produces( "application/json" )]
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserManager _userManager;
+
+        public UsersController( IUserManager userManager )
+        {
+            _userManager = userManager;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<User> users = _userManager.GetUsers();
+
+            string json = JsonSerializer.Serialize( users );
+
+            return new JsonResult( users );
+        }
+
+        // GET: api/Users/5
+        [HttpGet( "{id}" )]
+        public string Get( int id )
+        {
+            return "value";
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public void Post( [FromBody] string value )
+        {
+        }
+
+        // PUT: api/Users/5
+        [HttpPut( "{id}" )]
+        public void Put( int id, [FromBody] string value )
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete( "{id}" )]
+        public void Delete( int id )
+        {
+        }
+    }
+}
--- EquipmentManager/EquipmentManager/Startup.cs
+++ EquipmentManager/EquipmentManager/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 using UserService;
 
 namespace EquipmentManager
@@ -22,6 +25,18 @@ namespace EquipmentManager
         public void ConfigureServices( IServiceCollection services )
         {
             services.AddControllersWithViews();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc( "v1", new OpenApiInfo { Title = "Equipment Manager", Version = "v1" } );
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine( AppContext.BaseDirectory, xmlFile );
+                c.IncludeXmlComments( xmlPath );
+            } );
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles( configuration =>
              {
@@ -53,6 +68,17 @@ namespace EquipmentManager
                 app.UseSpaStaticFiles();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI( c =>
+            {
+                c.SwaggerEndpoint( "/swagger/v1/swagger.json", "Equipment Manager V1" );
+                c.RoutePrefix = string.Empty;
+            } );
+
             app.UseRouting();
 
             app.UseEndpoints( endpoints =>
@@ -64,10 +90,10 @@ namespace EquipmentManager
 
             app.UseSpa( spa =>
              {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                 spa.Options.SourcePath = "ClientApp";
 
                  if ( env.IsDevelopment() )
                  {
